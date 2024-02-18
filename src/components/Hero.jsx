779c7cd14eae8e51db924af393681e4fb5b53eee
meryem <meryemajmani33@gmail.com>
2024-02-18 05:29:36 +0100

    Fix bgImage issue

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 const Hero = () => {
   return (
     <>
-    <div style={{ height: 'calc(100vh - 80px)' }} className='w-full    bg-[url("https://www.daisyjewellery.com/cdn/shop/files/DoWhatYouLove_HomepageHero_draft1_3760x.jpg?v=1706802356")] bg-cover bg-center '>
+  
+    <div style={{ height: 'calc(100vh - 80px)' }} className='w-full    bg-hero bg-cover bg-center '>
 
-    </div>
+</div>
     </>
   )
 }
